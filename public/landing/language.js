8daeaa024239e94e69f3de48a4fa3a58e20c8e6c
Nicolò Valenza <nico.vale.spazio@gmail.com>
2023-07-16 14:31:04 +0200

    Aggiunta funzione multilinguaggio
    Modifica del file language.js
    Aggiunta di versioni inglese del testo in en.json
    Sistemazione della versione inglese di /index.html e /about/index.htmml

--- public/landing/language.js
+++ public/landing/language.js
@@ -1,17 +1,17 @@
 let language_url = {
     en: "en.json",
     it: "it.json",
-};
+};//File where traduction are located
 
 async function onload() {
     let language = "en";
     let url_lang = null;
     let urlQuery = new URLSearchParams(window.location.search);
-    url_lang = urlQuery.get("lang");
+    url_lang = urlQuery.get("lang");//read url wuey to check if is present languiage
     console.log(url_lang);
     if (url_lang == null && url_lang !== "en" && url_lang !== "it") {
         console.log("no lang");
-        let browserLang = navigator.language;
+        let browserLang = navigator.language;//check browser language
         if (browserLang == "it" || browserLang == "it-IT") {
             language = "it";
         } else {
@@ -20,8 +20,15 @@ async function onload() {
     } else {
         language = url_lang;
     }
-    let url = language_url[language];
+    let curr_path = window.location.pathname
+    if (curr_path.endsWith("/")) {
+        let url = curr_path + language_url[language];
+    }
+    else {
+        let url = curr_path + "/" + language_url[language];
+    }
     let response = await fetch(url);
+    console.log(url);
     let data = await response.json();
     Object.entries(data).forEach(([key, value]) => {
         let element = document.querySelector(`[content=${key}]`);
